b34db07fbea74396674a7cae36f78d08d74a6310
Kir Kup <k.bushmin@andersenlab.com>
2019-06-12 22:03:25 +0300

    for host

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,10 +9,8 @@ export const getInitState = () => dispatch => {
   const keys = Object.keys(localStorage).filter(
     key => key !== "loglevel:webpack-dev-server"
   );
-  const id = Math.max(
-    ...keys.map(key => Number(JSON.parse(localStorage[key]).id)),
-    0
-  );
+  const allIds = keys.map(key => Number(JSON.parse(localStorage[key]).id));
+  const maxId = Math.max(...allIds, 0);
   const initState = keys.map(key => JSON.parse(localStorage[key]));
-  dispatch(getTasks({ tasks: initState, currentId: id }));
+  dispatch(getTasks({ tasks: initState, currentId: maxId }));
 };
